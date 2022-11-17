111848d4c86c60ec4b05b133cb1a010529d69ff7
rodrigodelera <rodrigodelera02@gmail.com>
2022-11-17 12:10:21 +0100

    domain correct, always works

--- part1.py
+++ part1.py
@@ -15,17 +15,17 @@ class Student:
             if self.mobility == "R":
                 self.values = [1, 2, 3, 4, 13, 14, 15, 16]
             else:
-                for i in range(1, 16):
+                for i in range(1, 17):
                     self.values.append(i)
         else:
             if self.mobility == "R":
                 self.values = [17, 18, 19, 20]
             else:
-                for i in range(17, 32):
+                for i in range(17, 33):
                     self.values.append(i)
 
     def setSibling(self, std_vector):  # We only execute it if the student has a sibling
-        bro = std_vector[self.sibling]
+        bro = std_vector[(self.sibling)-1]
         if self.year > bro.year:
             self.values = [2, 3, 6, 7, 10, 11, 14, 15]
             bro.values = [1, 4, 5, 8, 9, 12, 13, 16]
@@ -48,6 +48,7 @@ class Student:
         print("tr: ", self.trouble)
         print("mob: ", self.mobility)
         print("sib: ", self.sibling)
+        print("values: ", self.values)
 
 def setDomain(student_vector):
     for student in student_vector:
@@ -75,6 +76,8 @@ def readFile(input_file):
 def main(inpath):
     student_vector = readFile(inpath)
     setDomain(student_vector)
+    for i in student_vector:
+        i.st_print()
 
 
 if __name__ == "__main__":
